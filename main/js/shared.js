816cdc1d27cc0d91715856125287e15769d6ff8f
Enkhtur <enkhtur.e@mak.mn>
2023-09-12 14:33:52 +0800

    2023-09-12 05

--- main/js/shared.js
+++ main/js/shared.js
@@ -12,6 +12,24 @@ window.fbAsyncInit = function() {
         cookie: true, // enable cookies to allow the server to access the session
         xfbml: true // parse XFBML
     });
+    FB.Event.subscribe('auth.statusChange', function(response) {
+        var loginButton = "<img src='img/log-in-with-facebook.png' alt='log in with Facebook' style='cursor:pointer; height: 46px;'' onclick='login();' />";
+        if (response.status === 'connected') {
+            getUserInfo();
+            document.getElementById("message").innerHTML += "Connected to Facebook";
+            //SUCCESS
+        } else if (response.status === 'not_authorized') {
+            document.getElementById("message").innerHTML += "Failed to Connect";
+            document.getElementById("status").innerHTML = loginButton;
+            //FAILED
+        } else {
+            console.log('auth.statusChange')
+            console.log(response)
+            document.getElementById("message").innerHTML += response.status
+            document.getElementById("status").innerHTML = loginButton;
+            //UNKNOWN ERROR
+        }
+      });
     FB.getLoginStatus(function(response) {
         var loginButton = "<img src='img/log-in-with-facebook.png' alt='log in with Facebook' style='cursor:pointer; height: 46px;'' onclick='login();' />";
         if (response.status === 'connected') {
